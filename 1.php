a32cd5f02e2ef7643de14d492e3811382e75c8dd
Joshua Hong <johong@ucalgary.ca>
2019-01-31 14:33:07 -0700

    Working json parser

--- 1.php
+++ 1.php
@@ -1,5 +1,22 @@
 <html>
     <body>
-    
+        <?php
+        $json_file = file_get_contents("CalgarySchools.geojson");
+        $calgaryschools = json_decode($json_file, $assoc = TRUE);
+
+        function iterateArray($array) {
+            foreach($array as $key=>$value) {
+                if (!is_array($value)) {
+                    echo "<p>Key=" . $key . " | Value=" . $value . "</p>";
+                    // echo "<p>$value</p>";
+                } else {
+                    echo "<p><em><strong>Key=" . $key . "</em></strong> </p>";
+                    iterateArray($value);
+                }
+            }
+        }
+
+        iterateArray($calgaryschools);
+        ?>
     </body>
 </html>
