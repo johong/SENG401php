803b3529701b7668ba838013b2c828fdd45ea2dc
Joshua Hong <johong@ucalgary.ca>
2019-02-12 15:24:54 -0700

    able to parse json and re-encode json

--- 1.php
+++ 1.php
@@ -1,22 +1,35 @@
-<html>
-    <body>
-        <?php
-        $json_file = file_get_contents("CalgarySchools.geojson");
-        $calgaryschools = json_decode($json_file, $assoc = TRUE);
+<?php
+$json_file = file_get_contents("CalgarySchools.geojson");
+$calgaryschools = json_decode($json_file, $assoc = TRUE);
 
-        function iterateArray($array) {
-            foreach($array as $key=>$value) {
-                if (!is_array($value)) {
-                    echo "<p>Key=" . $key . " | Value=" . $value . "</p>";
-                    // echo "<p>$value</p>";
-                } else {
-                    echo "<p><em><strong>Key=" . $key . "</em></strong> </p>";
-                    iterateArray($value);
-                }
-            }
+function iterateArray($array) {
+    $new_array = array();
+
+    foreach($array as $key => $value)
+    {
+        if (!is_array($value))
+        {
+            $new_array[$key] = $value;
+        } else {
+            $new_array[$key] = iterateArray($value);
         }
+    }
 
-        iterateArray($calgaryschools);
-        ?>
-    </body>
-</html>
+    return $new_array;
+}
+
+$calgaryschools_array = array();
+
+foreach($calgaryschools as $key => $value)
+{
+    if (!is_array($value))
+    {
+        $calgaryschools_array[$key] = $value;
+    } else {
+        $calgaryschools_array[$key] = iterateArray($value);
+    }
+}
+
+file_put_contents("CalgarySchoolsRemade.geojson", json_encode($calgaryschools_array));
+
+?>
